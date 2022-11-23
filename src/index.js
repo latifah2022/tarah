36ce9ad1ef3a605090762945d7eb72b04b036878
Latifah Zawedde <zawedde2021@gmail.com>
2022-11-23 14:02:54 -0800

    creatin BudgetContext.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 // import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BudgetContext } from "./context/BudgetContext"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BudgetContext>
+      <App />
+    </BudgetContext>
+    
   </React.StrictMode>,
   document.getElementById("root")
 );
